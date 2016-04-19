52cb3dbcc4585e67b29a95f292ffff0d69e90de3
Tjeerd <tj_heeringa@hotmail.com>
2016-04-19 19:48:56 +0200

    Placed placeholders at login

--- index.php
+++ index.php
@@ -11,15 +11,18 @@ $_SESSION['voted']=FALSE;
 if(!isset($_SESSION['voted'])){
 	$_SESSION['voted'] = FALSE;
 }
+if(isset($_SESSION['message'])){
+	ECHO "<script type='text/javascript'>alert('".$_SESSION['message']."');</script>";
+	unset($_SESSION['message']);
+}
 ?>
-
 </head>
 <body>
 	<div class="main">
 	<h2>Candidateboard poll</h2>
 		<form action="start_vote.php" method="post">
-			<input class="inputBox" name="myusername" type="text" value="">
-			<input class="inputBox" name="mypassword" type="text" value="">
+			<input class="inputBox" name="myusername" type="text" placeholder="Name">
+			<input class="inputBox" name="mypassword" type="text" placeholder="Access code">
 			<input name="submit" type="submit" value="Login">
 		</form>
 	</div>
